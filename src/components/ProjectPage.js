7f050fd14b966c3fca96774f93c3856be442b7c3
Luuk <luukhafkamp@gmail.com>
2019-01-20 00:38:07 +0100

    Display singular project

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
-const ProjectPage = () => (
-  <div>
-    <h1>Project test</h1>
-  </div>
+const ProjectPage = ({ match }) => (
+  <Query
+    query={gql`
+      {
+        project(language: en, slug: "${match.params.projectPage}") {
+          title
+          subtitle
+          excerpt
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>
+      if (error) return <p>Error</p>
+
+      const { title, subtitle, excerpt } = data.project
+
+      return <div>
+        <h1>{title}</h1>
+        <h3>{subtitle}</h3>
+        <p>{excerpt}</p>
+      </div>
+    }}
+    
+  </Query>
 )
 
 export default ProjectPage
