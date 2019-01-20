0fd28765e96acbc6e08f57d0ec3eed90d48874a9
Luuk <luukhafkamp@gmail.com>
2019-01-20 15:11:09 +0100

    Added styling for the project items

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom'
 const Project = ({ title, slug, tags }) => (
   <Link to={slug} className="project">
     <p>{title}</p>
-    <ul>
-      {tags.map(tag => <li key={tag}>{tag}</li>)}
-    </ul>
+    <div>
+      <ul>
+        {tags.map(tag => {
+          return <li key={tag}>{tag}</li>
+        })}
+      </ul>
+    </div>
   </Link>
 )
 
