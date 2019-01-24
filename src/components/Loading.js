d6cf5caf21f9940a38e0adc3c0fc1eeb07dda1ec
Luuk <luukhafkamp@gmail.com>
2019-01-24 21:42:12 +0100

    Class to classname

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => (
   <div className="loading">
-    <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
     <div className="yellow-bg"></div>
   </div>
 )
